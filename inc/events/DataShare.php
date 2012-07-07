9d59ac625485a328b1b9f1bef2522c6dbc232846
Leon <leon.savage92@live.co.uk>
2012-07-07 22:44:07 +0100

    Datashare update

--- inc/events/DataShare.php
+++ inc/events/DataShare.php
@@ -1,6 +1,9 @@
 <?php
 global $from, $c;
 $botfile = xload_config('botinfo');
+$DataShare = xload_config( 'dsMod' );
+if(is_array($DataShare)){
 if(strtolower($from)==strtolower($botfile['username'])) return;
 say( $from, $c);
+}
 ?>
